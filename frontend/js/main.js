ae76976445de3e47bcd2ffcc5aa962fdf4d28d5e
enriqueaguadoea-byte <enriqueaguado.ea@gmail.com>
2026-05-01 19:52:08 -0600

    new

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,3 +1,19 @@
 function openGame(gameName) {
-  window.location.href = `../games/${gameName}/index.html`;
+  const paths = {
+    "swipe-god": "/games/swipe-god/index.html",
+    "math-runner": "/games/math-runner/index.html"
+  };
+
+  // POP OUT WINDOW
+  const width = 1000;
+  const height = 700;
+
+  const left = (screen.width - width) / 2;
+  const top = (screen.height - height) / 2;
+
+  window.open(
+    paths[gameName],
+    "_blank",
+    `width=${width},height=${height},top=${top},left=${left}`
+  );
 }
